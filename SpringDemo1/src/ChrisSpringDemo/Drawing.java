83dbb93b53eb964f832c2583534e4183c195706e
ChrisWang <christianwang2sh@gmail.com>
2017-06-12 20:46:59 -0400

    initial commit

---
+++ SpringDemo1/src/ChrisSpringDemo/Drawing.java
@@ -0,0 +1,20 @@
+package ChrisSpringDemo;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
+public class Drawing {
+
+	public static void main(String[] args) {
+	
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Triangle triangle = (Triangle) context.getBean("triangle");
+        
+        triangle.draw();
+
+	}
+
+}
